977173535e4d5ace5c1ead547a49154434294168
Vikas Reddy <vikas2309reddy@gmail.com>
2024-02-24 11:15:30 +0530

    final push

--- src/components/CheckerPage.js
+++ src/components/CheckerPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import classes from './styles/Checkerpage.module.css';
 
 class CheckerPage extends Component {
@@ -9,11 +11,21 @@ class CheckerPage extends Component {
       uploadedImage: null,
       visible: false,
       resultImage: null,
+      classificationResult: null,
       loading: false,
       error: null,
-      countdown: 5, // Initial countdown value
+      inputImageSize: { width: 400, height: 400 }, // Initial size of input image
+      outputImageSize: { width: 400, height: 400 }, // Initial size of output image
     };
   }
+
+  toggleImageSize = (type) => {
+    this.setState((prevState) => ({
+      [`${type}ImageSize`]: prevState[`${type}ImageSize`].width === 400
+        ? { width: 650, height: 650 }
+        : { width: 400, height: 400 },
+    }));
+  };
 
   uploadFile = async (e) => {
     e.preventDefault();
@@ -21,6 +33,7 @@ class CheckerPage extends Component {
     this.setState({
       visible: false,
       resultImage: null, // Reset resultImage
+      classificationResult: null, // Reset classificationResult
       loading: true,
       error: null,
     });
@@ -35,22 +48,50 @@ class CheckerPage extends Component {
     formData.append('file', this.state.uploadedImage, this.state.uploadedImage.name);
 
     const API_ENDPOINT = "http://localhost:8000/tbdetection/";
+    const CLASSIFICATION_ENDPOINT = "http://localhost:8000/tbdetection/class";
 
     try {
-      const res = await axios.post(API_ENDPOINT, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-        responseType: 'arraybuffer',
-      });
+      // Send both requests simultaneously
+      const [res, classificationRes] = await Promise.all([
+        axios.post(API_ENDPOINT, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+          responseType: 'arraybuffer',
+        }),
+        axios.post(CLASSIFICATION_ENDPOINT, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          }
+        })
+      ]);
 
       const resultImage = new Blob([res.data], { type: 'image/jpeg' });
+      const classificationResult = classificationRes.data;
 
       this.setState({
         visible: true,
         resultImage: URL.createObjectURL(resultImage),
+        classificationResult: classificationResult,
         loading: false,
       });
+
+      // Show toast
+      if (classificationResult === "negative") {
+        toast.error("Classification Result: Positive", {
+          position: "top-right",
+          autoClose: 5000,
+          style: { marginTop: '30px', backgroundColor: 'red', color: 'white' }
+        });
+      } else {
+        toast.success("Classification Result: Negative", {
+          position: "top-right",
+          autoClose: 5000,
+          style: { marginTop: '30px', backgroundColor: 'green', color: 'white' }
+        });
+      }
+      
+
     } catch (error) {
       console.error(error);
       this.showError("Error processing image");
@@ -67,15 +108,8 @@ class CheckerPage extends Component {
   };
 
   showError = (errorMessage) => {
-    this.setState({ error: errorMessage, loading: false, countdown: 5 }); // Reset countdown and stop loading
-    const countdownInterval = setInterval(() => {
-      this.setState((prevState) => ({
-        countdown: prevState.countdown - 1,
-      }));
-    }, 1000);
-
+    this.setState({ error: errorMessage, loading: false }); // Stop loading
     setTimeout(() => {
-      clearInterval(countdownInterval); // Stop countdown
       this.setState({ error: null });
     }, 5000);
   };
@@ -83,17 +117,20 @@ class CheckerPage extends Component {
   render() {
     return (
       <div className="container">
-        {/* Error message with countdown */}
+        {/* Error message */}
         {this.state.error && (
           <div className="row">
             <div className="col">
               <div className="alert alert-danger" role="alert">
-                {this.state.error} ({this.state.countdown})
+                {this.state.error}
               </div>
             </div>
           </div>
         )}
-        
+
+        {/* Toast Container */}
+        <ToastContainer />
+
         {/* Main content */}
         <div className="row">
           <div className="col-md-12">
@@ -104,12 +141,12 @@ class CheckerPage extends Component {
 
               <br />
               <form
-            style={{
-              width: '60%',
-            }}
-            encType="multipart/form-data" // Ensure proper form submission for file upload
-            onSubmit={this.uploadFile}
-          >
+                style={{
+                  width: '60%',
+                }}
+                encType="multipart/form-data" // Ensure proper form submission for file upload
+                onSubmit={this.uploadFile}
+              >
                 <input
                   className="form-control"
                   type="file"
@@ -124,14 +161,14 @@ class CheckerPage extends Component {
             </div>
           </div>
           {this.state.loading && (
-          <div className="row">
-            <div className="col">
-              <div className={classes.results}>
-                <div className={classes.spinner}></div>
+            <div className="row">
+              <div className="col">
+                <div className={classes.results}>
+                  <div className={classes.spinner}></div>
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
 
           {/* Uploaded image */}
           {this.state.uploadedImage && (
@@ -143,9 +180,10 @@ class CheckerPage extends Component {
                     className={classes.image}
                     src={URL.createObjectURL(this.state.uploadedImage)}
                     alt=""
-                    width="400"
-                    height="400"
-                    style={{ border: '1px solid black', borderRadius: '12px' }}
+                    width={this.state.inputImageSize.width}
+                    height={this.state.inputImageSize.height}
+                    style={{ border: '1px solid black', borderRadius: '12px', width: this.state.inputImageSize.width + 'px', height: this.state.inputImageSize.height + 'px' }}
+                    onDoubleClick={() => this.toggleImageSize('input')}
                   />
                 </div>
               </div>
@@ -155,25 +193,24 @@ class CheckerPage extends Component {
           {/* Result display */}
           {this.state.visible && (
             <div className="col-md-6">
-            <div className={classes.results}>
-              <div className={classes.text}>Output X-ray</div>
-              <div className={classes.imageContainer}>
-                <img
-                  className={classes.image}
-                  src={this.state.resultImage}
-                  alt=""
-                  width="400"
-                  height="400"
-                  style={{ border: '1px solid black', borderRadius: '12px' }}
-                />
+              <div className={classes.results}>
+                <div className={classes.text}>Output X-ray</div>
+                <div className={classes.imageContainer}>
+                  <img
+                    className={classes.image}
+                    src={this.state.resultImage}
+                    alt=""
+                    width={this.state.outputImageSize.width}
+                    height={this.state.outputImageSize.height}
+                    style={{ border: '1px solid black', borderRadius: '12px', width: this.state.outputImageSize.width + 'px', height: this.state.outputImageSize.height + 'px' }}
+                    onDoubleClick={() => this.toggleImageSize('output')}
+                  />
+                </div>
               </div>
             </div>
-          </div>
           )}
-        </div>
 
-        {/* Loading spinner */}
-        
+        </div>
       </div>
     );
   }
